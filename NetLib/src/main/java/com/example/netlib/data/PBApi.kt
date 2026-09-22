4b234e74a4f84d52badf91ec00f35cc2f910e8ba
frens <liadovn@yandex.ru>
2026-09-22 11:19:16 +0300

    create PBApiSrvice object

---
+++ NetLib/src/main/java/com/example/netlib/data/PBApi.kt
@@ -0,0 +1,8 @@
+package com.example.netlib.data
+
+import io.ktor.client.HttpClient
+
+class PBApi(
+    private val client: HttpClient,
+) {
+}
--- NetLib/src/main/java/com/example/netlib/data/PBApiService.kt
+++ NetLib/src/main/java/com/example/netlib/data/PBApiService.kt
@@ -1,7 +1,51 @@
 package com.example.netlib.data
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.bearerAuth
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+
 object PBApiService {
 
     const val BASE_URL = "http://frens-project.tech:8099/api/"
+
+    var token: String? = null
+
+    val instance: PBApi by lazy {
+        PBApi(
+            client = HttpClient(OkHttp){
+                expectSuccess = true
+
+                install(ContentNegotiation){
+                    json(
+                        Json{
+                            ignoreUnknownKeys = true
+                            isLenient = true
+                            encodeDefaults = true
+                        }
+                    )
+                }
+
+                install(Logging){
+                    level = LogLevel.ALL
+                }
+
+                defaultRequest {
+                    url(BASE_URL)
+                    contentType(ContentType.Application.Json)
+                    token?.let{bearerAuth(it)}
+                }
+
+            }
+        )
+    }
 
 }
