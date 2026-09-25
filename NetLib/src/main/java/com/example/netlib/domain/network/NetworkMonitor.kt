61affc44ac9af8ca2182a485702c9da1660a165a
frens <liadovn@yandex.ru>
2026-09-25 10:55:03 +0300

    create netwrok monitor

---
+++ NetLib/src/main/java/com/example/netlib/domain/network/NetworkMonitor.kt
@@ -0,0 +1,5 @@
+package com.example.netlib.domain.network
+
+interface NetworkMonitor {
+    fun NetworkMonitor(): Boolean
+}
---
+++ NetLib/src/main/java/com/example/netlib/network/NetworkMonitorImpl.kt
@@ -0,0 +1,15 @@
+package com.example.netlib.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import com.example.netlib.domain.network.NetworkMonitor
+
+class NetworkMonitorImpl(private val context: Context): NetworkMonitor {
+    override fun NetworkMonitor(): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?:return false
+        val capabilities = connectivityManager.getNetworkCapabilities(network)?: return false
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+}
